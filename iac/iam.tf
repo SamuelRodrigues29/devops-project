cc53a055bd86b11736a53f25be0f32f5e52ad8d0
Samuel Rodrigues <samuel_souza87@hotmail.com>
2026-08-20 11:25:55 -0300

    implementing ecs roles

--- iac/iam.tf
+++ iac/iam.tf
@@ -81,3 +81,38 @@ resource "aws_iam_role_policy" "ecr_role_policy" {
     ]
   })
 }
+
+# role para ECS pegar a imagem do ECR 
+resource "aws_iam_role" "ECR_pull_image" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+# permissão de infra para o ECS
+resource "aws_iam_role" "ecs_infrastructure_role" {
+  name = "ecs-infrastructure-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ecs.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ecs_infrastructure_role_policy" {
+  role       = aws_iam_role.ecs_infrastructure_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSInfrastructureRoleforExpressGatewayServices"
+}
